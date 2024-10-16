aa82fd8cfd6abd2d6c038bfd78087be1c8e1b677
petia duskin <petr.dyuskin@mail.ru>
2024-10-16 17:09:07 +0500

    sort users by column

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -3,13 +3,17 @@ import React from 'react';
 interface Props {
     params: {
         slug: string[]
+    };
+    searchParams: {
+        sortOrder: string;
     }
 }
 
-const ProductPage = ({params: {slug}}: Props) => {
+const ProductPage = ({params: {slug}, searchParams: {sortOrder}}: Props) => {
     return (
         <div>
             ProductPage {slug}
+            SortOrder {sortOrder}
         </div>
     );
 };
--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,18 +1,38 @@
+import Link from "next/link";
+
 interface User {
     id: number;
     name: string;
     email: string;
 }
 
-const UserTable = async () => {
+type SortColumn = keyof Omit<User, 'id'>;
+
+interface Props {
+    sortOrder: SortColumn;
+}
+
+const UserTable = async ({sortOrder}: Props) => {
     const res = await fetch("https://jsonplaceholder.typicode.com/users", {cache: 'no-store'});
     const users: User[] = await res.json();
+
+    const sortUsers = (column: SortColumn) => {
+        if (column == null || column.length === 0) {
+            return;
+        }
+        users.sort((user1, user2) => {
+            return user1[column].localeCompare(user2[column])
+        });
+    }
+
+    sortUsers(sortOrder);
+
     return (
         <table className="table table-bordered">
             <thead>
             <tr>
-                <th>Name</th>
-                <th>Email</th>
+                <th><Link href="/users?sortOrder=name">Name</Link></th>
+                <th><Link href="/users?sortOrder=email">Email</Link></th>
             </tr>
             </thead>
             <tbody>
