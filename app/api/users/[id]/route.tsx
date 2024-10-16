43ee758ae8335a99840cc7d6c087a6014a7492b2
petia duskin <petr.dyuskin@mail.ru>
2024-10-16 19:00:38 +0500

    get user api

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,14 @@
+import {NextRequest, NextResponse} from "next/server";
+
+interface Props {
+    params: {
+        id: number;
+    }
+}
+
+export function GET(request: NextRequest, {params: {id}}: Props) {
+    if (id > 10)
+        return NextResponse.json({error: "User not found"}, {status: 404})
+    
+    return NextResponse.json({id: 1, name: "Mosh"})
+}
