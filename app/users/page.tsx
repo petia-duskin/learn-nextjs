ca46b1984323c666fdb8309d817e229373cd7c25
petia duskin <petr.dyuskin@mail.ru>
2024-10-16 13:44:11 +0500

    fetch data in server component
    Обращение к api и получение данных должно происходить в server components

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,18 @@
-const UsersPage = () => {
+interface User {
+    id: number;
+    name: string;
+}
+
+const UsersPage = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    const users: User[] = await res.json();
     return (
-        <div>UsersPage</div>
+        <>
+            <h1>Users</h1>
+            <ul>
+                {users.map(user => <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </>
     )
 }
 
