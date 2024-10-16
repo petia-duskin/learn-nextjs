344de7174cef4b93dc945f2b3a525821ddb7f8af
petia duskin <petr.dyuskin@mail.ru>
2024-10-16 16:41:58 +0500

    optional routes

---
+++ app/products/[[...slug]]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+    params: {
+        slug: string[]
+    }
+}
+
+const ProductPage = ({params: {slug}}: Props) => {
+    return (
+        <div>
+            ProductPage {slug}
+        </div>
+    );
+};
+
+export default ProductPage;
