e0546a4ea88b601a92b66da4933b49796bdd018b
petia duskin <petr.dyuskin@mail.ru>
2024-10-16 13:38:48 +0500

    client and server components difference
    Client компоненты рендерятся в браузере и имею доступ к хукам, всему привычному что есть в react.
    ServerComponents рендерятся на сервере, они быстрее, надежнее, но менее интерактивны, так как нету прямого доступа к клиенту

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const AddToCart = () => {
+    return (
+        <div>
+            <button onClick={() => console.log("Click")}>Add to card</button>
+        </div>
+    )
+}
+
+export default AddToCart;
