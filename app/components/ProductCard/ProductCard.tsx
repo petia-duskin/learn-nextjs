dcfcf293f609abec8a58644d89aef917a134cb60
petia duskin <petr.dyuskin@mail.ru>
2024-10-16 15:58:49 +0500

    tailwind css

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,9 +1,8 @@
 import AddToCart from "@/app/components/AddToCart";
-import styles from './Product.module.css'
 
 const ProductCard = () => {
     return (
-        <div className={styles.card}>
+        <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500 transition duration-200">
             <AddToCart />
         </div>
     )
